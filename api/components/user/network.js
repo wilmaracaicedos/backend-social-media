346ea4f169503f10af52e4cd01a845177097790c
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-10-07 14:28:14 -0500

    agregar consulta de usuarios

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -13,35 +13,29 @@ router.post('/', upsert);
 router.put('/', secure('update'), upsert);
 
 // Internal functions
-function list(req, res) {
+function list(req, res, next) {
   controller.list()
     .then((lista) => {
       response.success(req, res, lista, 200);
     })
-    .catch((err) => {
-      response.error(req, res, err.message, 500);
-    });
+    .catch(next);
 }
 
-function get(req, res) {
+function get(req, res, next) {
   controller.get(req.params.id)
     .then((user) => {
       response.success(req, res, user, 200);
     })
-    .catch((err) => {
-      response.error(req, res, err.message, 500);
-    });
+    .catch(next);
 };
 
-function upsert(req, res) {
+function upsert(req, res, next) {
   console.log(req.body);
   controller.upsert(req.body)
     .then((user) => {
       response.success(req, res, user, 201);
     })
-    .catch((err) => {
-      response.error(req, res, err.message, 500);
-    });
+    .catch(next);
 };
 
 module.exports = router;
--- config.js
+++ config.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
+
 module.exports = {
   api: {
-    port: process.env.API_PORT || 3000,
+    port: process.env.NODE_DOCKER_PORT || 3000,
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'notasecret!',
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    port: process.env.MYSQL_PORT,
   }
 }
